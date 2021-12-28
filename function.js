2d3972f2295601cfcc339e460a7962ece461fb6e
Mauricio Iván Molina Carrosio <imcarrosio@gmail.com>
2021-12-27 21:48:57 -0300

    Update function.js

--- function.js
+++ function.js
@@ -4,28 +4,23 @@ async function loadPrice() {
     let result = {}
     const response = await fetch(dataAPI_URL);
     result = await response.json();
+    let value = 0
+    console.log(value = value + 1)
     return result
 }
 
-function porfolio(coins, price){
-    return coins * price
-}
+
+
 
 document.addEventListener("DOMContentLoaded", async function (e) {
     const price = document.getElementById("price") 
     const priceUSD = document.getElementById("priceUSD")
-   /*  const enterBtm = document.getElementById("enter")
-    const enterQty = document.getElementById("usdqty") */
+  
     const tinyPriceAlgo = (await loadPrice()).data
     
-    const tinyAmount = 5658 + 19714
-    const USDAmount = 576
+    const tinyAmount = 5020 + 20000
+    const USDAmount = 520 + (9 * tinyPriceAlgo[3]) + 318 + 324
 
-   /*  enterBtm.onclick = function(){
-        USDAmount = parseInt(enterQty.value)
-    }
- */
-    
    
     let priceAlgo = Number(tinyPriceAlgo[0].toFixed(4)) + ' ALGO ' 
     let priceTiny = tinyPriceAlgo[0] * tinyPriceAlgo[3]
@@ -36,18 +31,21 @@ document.addEventListener("DOMContentLoaded", async function (e) {
     
     console.log("Gracias Pablo por la API <3")
 
-    window.addEventListener('load', function () {
-        // Your document is loaded.
-        var fetchInterval = 5000; // 5 seconds.
-      
-        // Invoke the request every 5 seconds.
-        setInterval(loadPrice, fetchInterval);
-      });
+   
 })
 
 
-
-
+/* thenano2412@gmail.com
+Clave
+61c9f10048decd000138bc7a
+Secreto
+El secreto de la API no se puede recuperar y se mostrará SOLO una vez. Manténgalo seguro.
+6806aedc-749d-4089-9ca6-9e4b965930ef
+Restricciones de API
+General
+Acceso IP
+Sin límite
+ */
 
 
 
